c95c316caf557403a88a324cd4d699fe15e03b20
JoaoMazzoni <joao.mazzoni@5by5.com>
2024-04-18 15:59:38 -0300

    Add project files.

---
+++ ImparOuPar/Program.cs
@@ -0,0 +1,25 @@
+﻿int n1;
+
+
+Console.WriteLine("Digite um número: ");
+n1 = int.Parse(Console.ReadLine());
+if (n1 == 0)
+{
+    Console.WriteLine("O número zero é um número neutro.");
+}
+
+else 
+{ 
+    if (n1%2 == 0)
+    {
+        Console.WriteLine("O número {0} é par.", n1);
+    }
+
+    else 
+    {
+        Console.WriteLine("O número {0} é ímpar.", n1);
+    }
+
+}
+
+Console.ReadKey();
